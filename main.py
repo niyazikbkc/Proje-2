a2d05b993cc25344206a57e0f57000e369fd03d0
niyazikbkc <niyazikbkc@gmail.com>
2024-08-19 14:47:44 +0300

    son düzenlemeler

--- main.py
+++ main.py
@@ -1,63 +1,99 @@
-import ast,time
+import ast, time
 
 def kisiekle():
-    ad = input("Rehbere Eklenecek Kişinin Adı")
-    soyad = input("Rehbere Eklencek Kişinin Soyadı")
-    no = input("Rehbere Eklenecek Kişinin Numaası")
-    kisi = {ad=="ADI",soyad=="Soyadı",no=="NUMARASI"}
-    dosya = open("rehber.listesi","a")
-    dosya.write(str(kisi)),
-    dosya.close()
+    time.sleep(1)
+    ad = input("Rehbere Eklenecek Kişinin Adı: ")
+    time.sleep(1)
+    soyad = input("Rehbere Eklenecek Kişinin Soyadı: ")
+    time.sleep(1)
+    no = input("Rehbere Eklenecek Kişinin Numarası: ")
+    kisi = {"ADI": ad, "SOYADI": soyad, "NUMARASI": no}
+    with open("rehber.listesi", "a") as dosya:
+        dosya.write(str(kisi) + "\n")
 
+def listele():
+    time.sleep(1)
+    with open("rehber.listesi", "r") as dosya:
+        bbb = dosya.read()
+        print(bbb)
 
-def listele() 
-    aaa=open("rehber.listesi","r")
-    bbb=read()
-    print(bbb)
+def ara():
+    time.sleep(1)
+    with open("rehber.listesi", "r") as dosya:
+        okunan = dosya.read()
+        cevirilen = ast.literal_eval("[" + okunan.replace("\n", ",")[:-1] + "]")
+        time.sleep(1)
+        aranan = input("Aranan İsim: ")
+        for a in cevirilen:
+            if a["ADI"] == aranan:
+                print(a)
 
-
-
-def ara ()
-        with open ("rehber.listesi","r") as dosya :
-            okunan = dosya.read()
-            cevirilen = ast.literal_eval(dosya)
-            aranan = input (" Aranan İsim : ")
-            for a in cevirilen
-            if a ["ADI"]== aranan :
-                print (a)
-
-def duzelt ()
-     with open ("rehber.listesi","r") as dosya :
-          okunan = dosya.read()
-          cevirilen = ast.literal_eval(dosya)
-          aranan = input (" Düzeltilecek İsim : ")
-          dosya.close()
-
-          with open ("rehber.list","r") as dosya
-          for a in cevirilen 
-          if a ["ADI"]== aranan :
-               print(a)
-          yenisim = input (" Düzeltilen Kişinin Rehberdeki Yeni İsmi : ")     
-          yenisoyad = input ("Düzeltilen Kişinin Rehberdeki Yeni Soyadı :")
-          yenino = input ("Düzeltilen Kişinin Rehberdeki Yeni Numarası : ")
-          a["ADI"]=yeniAd
-          a["NUMARASI"]=yeniNo
-     dosya.write(f"{str(a)},")
-
+def duzelt():
+    time.sleep(1)
+    with open("rehber.listesi", "r") as dosya:
+        okunan = dosya.read()
+        cevirilen = ast.literal_eval("[" + okunan.replace("\n", ",")[:-1] + "]")
+        time.sleep(1)
+        aranan = input("Düzeltilecek İsim: ")
+    
+    with open("rehber.listesi", "w") as dosya:
+        for a in cevirilen:
+            if a["ADI"] == aranan:
+                print(a)
+                time.sleep(1)
+                a["ADI"] = input("Düzeltilen Kişinin Rehberdeki Yeni İsmi: ")
+                time.sleep(1)
+                a["SOYADI"] = input("Düzeltilen Kişinin Rehberdeki Yeni Soyadı: ")
+                time.sleep(1)
+                a["NUMARASI"] = input("Düzeltilen Kişinin Rehberdeki Yeni Numarası: ")
+            dosya.write(str(a) + "\n")
 
 def sil():
-    with open("rehber.xx","r") as dosya:
+    time.sleep(1)
+    with open("rehber.listesi", "r") as dosya:
         okunan = dosya.read()
-    cevirilen = ast.literal_eval(okunan)
-    aranan = input("Silinecek isim:")
-    dosya.close()
-    with open("rehber.listesi","w") as dosya:
+        cevirilen = ast.literal_eval("[" + okunan.replace("\n", ",")[:-1] + "]")
+    
+    time.sleep(1)
+    aranan = input("Silinecek isim: ")
+    with open("rehber.listesi", "w") as dosya:
         for a in cevirilen:
             if a["ADI"] != aranan:
-                dosya.write(f"{str(a)},")
-                
+                dosya.write(str(a) + "\n")
 
+def menu():
+    time.sleep(1)
+    print("╔═══════════════════╗")
+    print("║ REHBER UYGULAMASI ║")
+    print("║1-Kişi Ekle        ║")
+    print("║2-Listele          ║")
+    print("║3-Ara              ║")
+    print("║4-Düzelt           ║")
+    print("║5-Sil              ║")
+    print("║0-Çıkış            ║")
+    print("╚═══════════════════╝")
+    
+    secim = input("Seçiminiz nedir? ")
+    if secim == "1":
+        kisiekle()
+        menu()
+    elif secim == "2":
+        listele()
+        menu()
+    elif secim == "3":
+        ara()
+        menu()
+    elif secim == "4":
+        duzelt()
+        menu()
+    elif secim == "5":
+        sil()
+        menu()
+    elif secim == "0":
+        print("Çıkış yapılıyor...")
+        time.sleep(1)
 
+menu()
 
 
 
