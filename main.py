bcab0a64d222b5395dc041554404ba07d049fa95
niyazikbkc <niyazikbkc@gmail.com>
2024-08-19 13:40:08 +0300

    DUZELT KOMUTU

--- main.py
+++ main.py
@@ -4,19 +4,56 @@ def kisiekle():
     ad = input("Rehbere Eklenecek Kişinin Adı")
     soyad = input("Rehbere Eklencek Kişinin Soyadı")
     no = input("Rehbere Eklenecek Kişinin Numaası")
-    kisi = {ad="ADI",soyad="Soyadı",no="NUMARASI"}
+    kisi = {ad=="ADI",soyad=="Soyadı",no=="NUMARASI"}
     dosya = open("rehber.listesi","a")
     dosya.write(str(kisi)),
     dosya.close()
 
 
-def listele()
+def listele() 
     aaa=open("rehber.listesi","r")
     bbb=read()
     print(bbb)
 
 
-    def ara ()
+
+def ara ()
+        with open ("rehber.listesi","r") as dosya :
+            okunan = dosya.read()
+            cevirilen = ast.literal_eval(dosya)
+            aranan = input (" Aranan İsim : ")
+            for a in cevirilen
+            if a ["ADI"]== aranan :
+                print (a)
+
+def duzelt ()
+     with open ("rehber.listesi","r") as dosya :
+          okunan = dosya.read()
+          cevirilen = ast.literal_eval(dosya)
+          aranan = input (" Düzeltilecek İsim : ")
+          dosya.close()
+
+          with open ("rehber.list","r") as dosya
+          for a in cevirilen 
+          if a ["ADI"]== aranan :
+               print(a)
+          yenisim = input (" Düzeltilen Kişinin Rehberdeki Yeni İsmi : ")     
+          yenisoyad = input ("Düzeltilen Kişinin Rehberdeki Yeni Soyadı :")
+          yenino = input ("Düzeltilen Kişinin Rehberdeki Yeni Numarası : ")
+          a["ADI"]=yeniAd
+          a["NUMARASI"]=yeniNo
+     dosya.write(f"{str(a)},")
+
+
+
+
+
+
+          
+
+
+            
+
 
 
 
